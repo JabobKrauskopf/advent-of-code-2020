ce89aa76091134d12bb920e0dab7102383958bea
Jakob Kraus <jakob.kraus@limebit.de>
2020-12-13 18:47:46 +0100

    Refactored day_11

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -1,26 +1,22 @@
 from copy import deepcopy
 
-with open("day_11/input.txt", "r") as myfile:
-    data = [[list(x.replace("\n", "")) for x in myfile.readlines()]]
-    data.append(deepcopy(data[0]))
-
 # Part One
 
 
-def is_safe(index):
+def is_safe(index, seats):
     if index[0] < 0 or index[1] < 0:
         return False
     try:
-        data[0][index[1]][index[0]]
+        seats[index[1]][index[0]]
         return True
     except IndexError:
         return False
 
 
-def check_seat(index_x, index_y):
-    occupied_count = sum(
+def occupied_count_part_one(index_x, index_y, seats):
+    return sum(
         [
-            1 if data[0][x[1]][x[0]] == "#" else 0
+            1 if seats[0][x[1]][x[0]] == "#" else 0
             for x in [
                 x
                 for x in [
@@ -33,156 +29,119 @@ def check_seat(index_x, index_y):
                     (index_x, index_y + 1),
                     (index_x + 1, index_y + 1),
                 ]
-                if is_safe(x)
+                if is_safe(x, seats[0])
             ]
         ]
     )
-    changes = False
-    if data[0][index_y][index_x] == "L" and occupied_count == 0:
-        data[1][index_y][index_x] = "#"
-        changes = True
-    elif data[0][index_y][index_x] == "#" and occupied_count >= 4:
-        data[1][index_y][index_x] = "L"
-        changes = True
-    return changes
 
-
-stable = False
-
-while not stable:
-    did_change = []
-    for index_y in range(len(data[0])):
-        for index_x in range(len(data[0][index_y])):
-            did_change.append(check_seat(index_x, index_y))
-    stable = not (True in did_change)
-    data[0] = deepcopy(data[1])
-
-counter2 = 0
-for index_y in range(len(data[0])):
-    for index_x in range(len(data[0][index_y])):
-        if data[0][index_y][index_x] == "#":
-            counter2 += 1
-
-print(counter2)
 
 # Part Two
 
-with open("day_11/input.txt", "r") as myfile:
-    data = [[list(x.replace("\n", "")) for x in myfile.readlines()]]
-    data.append(deepcopy(data[0]))
 
-
-def get_visible_seats(index_x, index_y):
+def get_visible_seats(index_x, index_y, seats):
     indices = []
-    counter = 1
-    while True:
-        if is_safe((index_x - counter, index_y)):
-            if data[0][index_y][index_x - counter] != ".":
-                indices.append((index_x - counter, index_y))
+    index_multiplier = [
+        (-1, 0),
+        (1, 0),
+        (-1, -1),
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+    ]
+    for multiplier in index_multiplier:
+        counter = 1
+        while True:
+            if is_safe(
+                (
+                    index_x + (counter * multiplier[0]),
+                    index_y + (counter * multiplier[1]),
+                ),
+                seats,
+            ):
+                if (
+                    seats[index_y + (counter * multiplier[1])][
+                        index_x + (counter * multiplier[0])
+                    ]
+                    != "."
+                ):
+                    indices.append(
+                        (
+                            index_x + (counter * multiplier[0]),
+                            index_y + (counter * multiplier[1]),
+                        )
+                    )
+                    break
+            else:
                 break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x + counter, index_y)):
-            if data[0][index_y][index_x + counter] != ".":
-                indices.append((index_x + counter, index_y))
-                break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x - counter, index_y - counter)):
-            if data[0][index_y - counter][index_x - counter] != ".":
-                indices.append((index_x - counter, index_y - counter))
-                break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x + counter, index_y + counter)):
-            if data[0][index_y + counter][index_x + counter] != ".":
-                indices.append((index_x + counter, index_y + counter))
-                break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x + counter, index_y - counter)):
-            if data[0][index_y - counter][index_x + counter] != ".":
-                indices.append((index_x + counter, index_y - counter))
-                break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x - counter, index_y + counter)):
-            if data[0][index_y + counter][index_x - counter] != ".":
-                indices.append((index_x - counter, index_y + counter))
-                break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x, index_y - counter)):
-            if data[0][index_y - counter][index_x] != ".":
-                indices.append((index_x, index_y - counter))
-                break
-        else:
-            break
-        counter += 1
-    counter = 1
-    while True:
-        if is_safe((index_x, index_y + counter)):
-            if data[0][index_y + counter][index_x] != ".":
-                indices.append((index_x, index_y + counter))
-                break
-        else:
-            break
-        counter += 1
+            counter += 1
     return indices
 
 
-# print(get_visible_seats(3, 4))
-
-
-def check_seat(index_x, index_y):
-    occupied_count = sum(
+def occupied_count_part_two(index_x, index_y, seats):
+    return sum(
         [
-            1 if data[0][x[1]][x[0]] == "#" else 0
-            for x in get_visible_seats(index_x, index_y)
+            1 if seats[0][x[1]][x[0]] == "#" else 0
+            for x in get_visible_seats(index_x, index_y, seats[0])
         ]
     )
-    changes = False
-    if data[0][index_y][index_x] == "L" and occupied_count == 0:
-        data[1][index_y][index_x] = "#"
-        changes = True
-    elif data[0][index_y][index_x] == "#" and occupied_count >= 5:
-        data[1][index_y][index_x] = "L"
-        changes = True
-    return changes
 
 
-stable = False
+def simulate_seats_general(
+    index_x, index_y, seats, occupied_count, occupied_count_limit
+):
+    seats[1][index_y][index_x] = (
+        "#"
+        if seats[0][index_y][index_x] == "L" and occupied_count == 0
+        else (
+            "L"
+            if seats[0][index_y][index_x] == "#"
+            and occupied_count >= occupied_count_limit
+            else seats[1][index_y][index_x]
+        )
+    )
+    return seats[1][index_y][index_x] != seats[0][index_y][index_x]
 
-while not stable:
-    did_change = []
-    for index_y in range(len(data[0])):
-        for index_x in range(len(data[0][index_y])):
-            did_change.append(check_seat(index_x, index_y))
-    stable = not (True in did_change)
-    data[0] = deepcopy(data[1])
 
-counter2 = 0
-for index_y in range(len(data[0])):
-    for index_x in range(len(data[0][index_y])):
-        if data[0][index_y][index_x] == "#":
-            counter2 += 1
+def simulate(function, data, occupied_count_limit):
+    data = [data, deepcopy(data)]
+    did_change = 1
+    while did_change > 0:
+        did_change = sum(
+            [
+                simulate_seats_general(
+                    index_x,
+                    index_y,
+                    data,
+                    function(index_x, index_y, data),
+                    occupied_count_limit
+                )
+                for index_y in range(len(data[0]))
+                for index_x in range(len(data[0][index_y]))
+            ]
+        )
+        data[0] = deepcopy(data[1])
 
-print(counter2)
+    return sum(
+        [
+            1
+            for index_y in range(len(data[0]))
+            for index_x in range(len(data[0][index_y]))
+            if data[0][index_y][index_x] == "#"
+        ]
+    )
+
+
+print(
+    simulate(
+        occupied_count_part_one,
+        [list(x.replace("\n", "")) for x in open("day_11/input.txt", "r").readlines()],
+        4,
+    ),
+    simulate(
+        occupied_count_part_two,
+        [list(x.replace("\n", "")) for x in open("day_11/input.txt", "r").readlines()],
+        5,
+    ),
+)
